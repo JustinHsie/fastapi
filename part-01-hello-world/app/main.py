8c32df1926420fe4705e85d2bb5af2811a45eba2
Justin Hsie <justinhsie1001@gmail.com>
2022-04-21 10:09:40 -0400

    Add comments

--- part-01-hello-world/app/main.py
+++ part-01-hello-world/app/main.py
@@ -1,24 +1,25 @@
 from fastapi import FastAPI, APIRouter
 
+# Instantiate FastAPI object
+app = FastAPI(
+    title="Recipe API", openapi_url="/openapi.json"
+)
 
-app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
-
+# Instantiate APIRouter object
 api_router = APIRouter()
 
-
+# Define GET route by adding decorator to root()
 @api_router.get("/", status_code=200)
 def root() -> dict:
     """
-    Root GET
+    Root Get
     """
     return {"msg": "Hello, World!"}
 
-
+# Register APIRouter api_router object with FastAPI app object
 app.include_router(api_router)
 
-
+# Runs in debug mode if module is called directly ie run with python
 if __name__ == "__main__":
-    # Use this for debugging purposes only
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
